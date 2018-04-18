172417605ed193f839431edfcec7f7d19b70f014
adam n <adam.nawroc@gmail.com>
2018-04-18 13:24:10 +0000

    automatically backed up by learn

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -29,7 +29,7 @@ class Boat < ActiveRecord::Base
 
   def self.sailboats
     # idk
-    Deal.select{|d| d.user.roles.include?("developer")}
+    Classification.select{|d| d.user.roles.include?("developer")}
 
     # make it in classification then chain onto that here?
 
