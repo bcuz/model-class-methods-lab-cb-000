098bb09edfc67bf26ccc6af8a283ad94744a0308
adam n <adam.nawroc@gmail.com>
2018-04-18 11:58:44 +0000

    progress

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -2,4 +2,11 @@ class Boat < ActiveRecord::Base
   belongs_to  :captain
   has_many    :boat_classifications
   has_many    :classifications, through: :boat_classifications
+
+  def self.first_five
+    # find(:all, :order => "id asc", :limit => 5)
+    # self.find(:all, :limit => 5)
+
+    order(id: :asc).limit(5)
+  end
 end
